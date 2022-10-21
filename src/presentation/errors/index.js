34a4ebeb944629db0a1b4e2f673f1c45738444ec
Davison Mota <43253853+davisonmota@users.noreply.github.com>
2022-10-21 10:33:50 -0300

    move generic erros to utils layer

--- src/presentation/errors/index.js
+++ src/presentation/errors/index.js
@@ -1,11 +1,7 @@
-const MissingParamError = require('./missing-param-error')
-const InvalidParamError = require('./invalid-param-error')
 const UnauthorizeError = require('./unauthorize-error')
 const ServerError = require('./server-error')
 
 module.exports = {
-  MissingParamError,
-  InvalidParamError,
   UnauthorizeError,
   ServerError
 }
