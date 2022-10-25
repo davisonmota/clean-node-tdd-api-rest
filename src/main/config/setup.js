0ce34ee4c3419d0a518ac9cd112c6d49893857e6
Davison Mota <43253853+davisonmota@users.noreply.github.com>
2022-10-24 21:10:53 -0300

    ensure requests content type as json by default

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,7 +1,10 @@
 const cors = require('../middleware/cors')
 const jsonParser = require('../middleware/json-parser')
+const contentType = require('../middleware/content-type')
+
 module.exports = app => {
   app.disable('x-powered-by') // disable default header express
   app.use(cors)
   app.use(jsonParser)
+  app.use(contentType)
 }
